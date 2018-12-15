d0a72cedb10b4c27d92d5c590895747f969b3555
Ryuya Nishino <ryuyasamaaaa@gmail.com>
2018-12-15 16:31:52 +0900

    fix tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,30 +1,40 @@
 class TasksController < ApplicationController
 
-  def index
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @tasks = Task.all
   end
 
   def new
-
   end
 
-  def create   
-
+  def create
+    Task.create(task_params)
   end
 
   def show
-    
   end
 
   def edit
-    
   end
 
   def update
-    
+    @task.update(task_params)
   end
 
   def destroy
-    
+    @task.destroy
   end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.permit(:title, :deadline)
+  end
+
 end
