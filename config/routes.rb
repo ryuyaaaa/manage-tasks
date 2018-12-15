310b1e8126215b8f1cfafee7e225ae4f1ab9b156
Ryuya Nishino <ryuyasamaaaa@gmail.com>
2018-12-15 15:44:36 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  root 'tasks#index'
   devise_for :user
+  root 'tasks#index'
+  get 'tasks' => 'tasks#index'
+  get 'tasks/new' => 'tasks#new'
+  post 'tasks' => 'tasks#create'
+  get 'tasks/:id' => 'tasks#show'
+  get 'tasks/:id/edit' => 'tasks#edit'
+  patch 'tasks/:id' => 'tasks#update'
+  delete 'tasks/:id' => 'tasks#destroy'
+
 end
